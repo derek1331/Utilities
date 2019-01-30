7b73ecd9cdc33817401a761e33835a1558dcaf7f
derek1331 <1331derek@gmail.com>
2019-01-30 11:35:44 -0700

    buggy includes utility

--- utilities/src/components/includes/includes.js
+++ utilities/src/components/includes/includes.js
@@ -6,26 +6,38 @@ const derekInfo = {
     Answers: [['cat','dog','rabbit','fish','bearded dragon',],["jimmy john's",'1-800-got-junk?']]
 }
 
+// not actualy an array
 class Includes extends React.Component{
-
-    componentWillMount(){
-        console.log(derekInfo.Questions[1])
-    }
 
     state = {
         answers: ''
     }
 
-    includesFunction (array,value) { 
-        array.includes(value)
+    // include button
+    includesFunction = () => { 
+        // value of input
+        const value = document.getElementById('inputIncludes').value
+        const resultSpan = document.getElementById('spanIncludesResult')
+        const result = this.state.answers.includes(value)
+        resultSpan.textContent = result
     }
+
+    // includesFunction = (array,value) => { 
+    //     array.includes(value)
+    // }
 
     selectFunction = (e) => {
         //select input
         // this.setState({
         //     answers: 
         // })
-        console.log(`this is`, this)
+        // event.target.value
+        const selectedValue = document.getElementById('select').value
+        this.setState({
+            answers: selectedValue
+        })
+  
+
         //update state
         //button runs function that run includes on state.answers
         // do i need state.questions?
@@ -34,13 +46,15 @@ class Includes extends React.Component{
     render() {
         return(
             <div>
-                <select >
-                    <option onSelect={this.selectFunction} value={derekInfo.Answers[0]}>{derekInfo.Questions[0]}</option>
-                    <option onSelect={this.selectFunction} value={derekInfo.Answers[1]}>{derekInfo.Questions[1]}</option>
+                <select id='select' onChange={this.selectFunction}>
+                    <option value={derekInfo.Answers[0]}>{derekInfo.Questions[0]}</option>
+                    <option value={derekInfo.Answers[1]}>{derekInfo.Questions[1]}</option>
                 </select>
                 <br></br>
-                <input type='text' className='inputIncludes' id='inputIncludes'></input>
-                <button className='buttonIncludes' id='buttonIncludes'>Search</button>
+                <input name='includes' value={this.state.includes} type='text' className='inputIncludes' id='inputIncludes'></input>
+                <button onClick={this.includesFunction} className='buttonIncludes' id='buttonIncludes'>Search</button>
+                <br></br>
+                <span className='spanIncludesResult' id='spanIncludesResult'></span>
 
             </div>
         )
