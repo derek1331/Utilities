e31b989657be0eb67739e10b6aa2c4155e8e7921
derek1331 <1331derek@gmail.com>
2019-01-30 21:31:53 -0700

    videoPlayer dailymotion only so far

--- utilities/src/App.js
+++ utilities/src/App.js
@@ -5,12 +5,13 @@ import Search from './components/search'
 import Header from './components/header'
 import ButtonColorChanger from './components/buttonColorChanger'
 import Includes from './components/includes'
+import VideoPlayer from './components/videoPlayer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Includes></Includes>     
+        <VideoPlayer></VideoPlayer>
       </div>
     );
   }
